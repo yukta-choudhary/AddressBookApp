41d28933002364bdf35b020c13c6571e8fcd7a68
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-03-07 15:59:37 +0530

    [Yukta]: added Contact

---
+++ src/main/java/com/addressbookapp/model/Contact.java
@@ -0,0 +1,20 @@
+package com.addressbookapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Contact {
+
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+	private String email;
+}
