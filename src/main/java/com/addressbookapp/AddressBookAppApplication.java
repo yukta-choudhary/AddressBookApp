45a79c1e2d6350483fffca9cf5672af8cc4f4f69
yukta-2k23 <yuktachoudhary2k7@gmail.com>
2026-03-07 15:58:43 +0530

    [Yukta]: modified AddressBookAppApplication

--- src/main/java/com/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/addressbookapp/AddressBookAppApplication.java
@@ -6,17 +6,21 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.addressbookapp.model.Contact;
+
 @SpringBootApplication
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication app = new SpringApplication(AddressBookAppApplication.class);
-		app.setWebApplicationType(WebApplicationType.NONE);
-
-		ConfigurableApplicationContext context = app.run(args);
+		SpringApplication.run(AddressBookAppApplication.class, args);
 
 		System.out.println("Welcome to Address Book");
+		
+		Contact contact = new Contact("John", "Doe", "Indrapuri", "Bhopal", "Madhya Pradesh", "462022",
+				"9876543210", "johndoe@email.com");
+
+		System.out.println(contact);
 
 	}
 }
